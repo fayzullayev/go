cc0f74acf941398fd53fca748b3143b07bdf6567
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-05-11 15:53:21 +0500

    Api creation have ended

--- gin/medium-1/main.go
+++ gin/medium-1/main.go
@@ -6,14 +6,56 @@ import (
 	"net/http"
 )
 
+var books = []Book{
+	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
+	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
+	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
+}
+
 func main() {
-	var router *gin.Engine
+	var r *gin.Engine
 
-	router = gin.Default()
+	r = gin.Default()
 
-	router.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{"data": "Hello"})
-	})
+	r.GET("/books", getAllBooks)
+	r.POST("/books", createBook)
+	r.DELETE("/books/:id", deleteBook)
 
-	log.Fatal(router.Run(":8008"))
+	log.Fatal(r.Run(":8008"))
+}
+
+type Book struct {
+	ID     string `json:"ids"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func getAllBooks(c *gin.Context) {
+	c.JSON(http.StatusOK, books)
+}
+
+func createBook(c *gin.Context) {
+	var book Book
+
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": "Wrong fields"})
+		return
+	}
+
+	books = append(books, book)
+
+	c.JSON(http.StatusCreated, book)
+}
+
+func deleteBook(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, a := range books {
+		if a.ID == id {
+			books = append(books[:i], books[i+1:]...)
+			break
+		}
+	}
+
+	c.Status(http.StatusNoContent)
 }
