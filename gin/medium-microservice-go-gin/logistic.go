7023fc3174a2a98ef79bfca63c8a69ff347c3026
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-05-12 16:50:39 +0500

    microservice

---
+++ gin/medium-microservice-go-gin/logistic.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-resty/resty/v2"
+)
+
+type EmailJob struct {
+	Sender  string `json:"sender" binding:"required"`
+	JobId   int    `json:"job_id" binding:"gte=0"`
+	Subject string `json:"subject" binding:"required"`
+	Email   string `json:"email" binding:"required"`
+	Body    string `json:"body" binding:"required"`
+}
+
+type LogisticsJob struct {
+	Type  string `json:"type" binding:"required"`
+	Email string `json:"email" binding:"required"`
+	Item  string `json:"item" binding:"required"`
+}
+
+func createEmailJob(emailJob EmailJob) EmailJob {
+	client := resty.New()
+
+	var p EmailJob
+
+	_, err := client.R().
+		SetBody(emailJob).
+		SetResult(&p).
+		Post("http://localhost:9000/send-email")
+
+	if err != nil {
+		log.Println("EmailServer: unable to connect EmailService")
+		return EmailJob{}
+	}
+	log.Printf("EmailServer: created email job #%v via EmailService", p.JobId)
+	return p
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/status", func(c *gin.Context) {
+		c.JSON(200, "Logistics Server Running")
+	})
+
+	router.POST("/process", func(c *gin.Context) {
+
+		var logistics LogisticsJob
+
+		if err := c.ShouldBindJSON(&logistics); err != nil {
+			c.JSON(400, gin.H{"error": "Invalid input!", "er": err})
+			return
+		}
+
+		//processing
+		emailData := EmailJob{
+			Sender:  "abc@logistics.com",
+			Subject: "Item" + logistics.Type,
+			Email:   logistics.Email,
+			Body:    logistics.Item,
+		}
+		//save this data in database
+
+		//calling email server
+
+		result := createEmailJob(emailData)
+
+		c.JSON(200, result)
+
+	})
+
+	log.Fatal(router.Run(":9001"))
+}
---
+++ gin/medium-microservice-go-gin/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"math/rand"
+	"time"
+)
+
+type EmailJob struct {
+	Sender  string `json:"sender" binding:"required"`
+	JobId   int    `json:"job_id" binding:"gte=0"`
+	Subject string `json:"subject" binding:"required"`
+	Email   string `json:"email" binding:"required"`
+	Body    string `json:"body" binding:"required"`
+}
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/status", func(c *gin.Context) {
+		c.JSON(200, gin.H{"data": "Email Server Running"})
+	})
+
+	router.POST("/send-email", func(c *gin.Context) {
+		var emailHandler EmailJob
+		if err := c.ShouldBindJSON(&emailHandler); err != nil {
+			c.JSON(400, gin.H{"error": "Invalid input!", "er": err})
+			return
+		}
+
+		log.Printf("Email Service: creating new Email job #%v...", emailHandler.Sender)
+		rand.Seed(time.Now().UnixNano())
+		emailHandler.JobId = rand.Intn(1000)
+		log.Printf("EmailService: created print job #%v", emailHandler.JobId)
+
+		//send email here
+		// sendEmail(emailHandler.Subject, emailHandler.Body+" has been shipped")
+		//
+		c.JSON(200, emailHandler)
+	})
+
+	log.Fatal(router.Run(":9000"))
+}
