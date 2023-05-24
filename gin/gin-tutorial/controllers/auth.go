f77a5c1fa6338c0600a145c866b8df1100d9cc25
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-05-24 12:14:28 +0500

    jwt end

--- gin/gin-tutorial/controllers/auth.go
+++ gin/gin-tutorial/controllers/auth.go
@@ -2,17 +2,18 @@ package controllers
 
 import (
 	"gin-tutorial/models"
+	"gin-tutorial/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-type RegisterInput struct {
+type UserInput struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
 func Register(c *gin.Context) {
-	var input RegisterInput
+	var input UserInput
 
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
@@ -38,4 +39,47 @@ func Register(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"data": saveUser,
 	})
+}
+
+func Login(c *gin.Context) {
+	var input UserInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	user := models.User{
+		Username: input.Username,
+		Password: input.Password,
+	}
+
+	token, err := models.LoginCheck(user.Username, user.Password)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username or password is incorrect."})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
+
+func CurrentUser(c *gin.Context) {
+
+	userId, err := utils.ExtractTokenID(c)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	u, err := models.GetUserByID(userId)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "success", "data": u})
 }
--- gin/gin-tutorial/main.go
+++ gin/gin-tutorial/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gin-tutorial/controllers"
+	"gin-tutorial/middlewares"
 	"gin-tutorial/models"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -16,6 +17,12 @@ func main() {
 	public := router.Group("/api")
 
 	public.POST("/register", controllers.Register)
+	public.POST("/login", controllers.Login)
+
+	protected := router.Group("/api/admin")
+	protected.Use(middlewares.JwtAuthMiddleware())
+
+	protected.GET("/user", controllers.CurrentUser)
 
 	log.Fatal(router.Run(":9999"))
 
