47284998189045129360328a2883193c6713c1cc
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-06-14 09:53:22 +0500

    Update main.go

--- w3go/main.go
+++ w3go/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var i string = "Hello"
-	var j int = 15
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+	// Original slice
+	fmt.Printf("numbers = %v\n", numbers)
+	fmt.Printf("length = %d\n", len(numbers))
+	fmt.Printf("capacity = %d\n", cap(numbers))
 
-	fmt.Printf("i has value %v and type: %T\n", i, i)
-	fmt.Printf("j has %% value %v and type: %T\n", j, j)
-	fmt.Printf("%-40d\n", -10)
+	// Create copy with only needed numbers
+	neededNumbers := numbers[:len(numbers)-10]
+	numbersCopy := make([]int, len(neededNumbers))
+	copy(numbersCopy, neededNumbers)
+
+	fmt.Printf("numbersCopy = %v\n", numbersCopy)
+	fmt.Printf("length = %d\n", len(numbersCopy))
+	fmt.Printf("capacity = %d\n", cap(numbersCopy))
 }
