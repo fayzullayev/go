50eeb440435cc9f1e912e1df86a33dca5729ffd8
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-06-16 16:52:55 +0500

    Update main.go

--- w3go/main.go
+++ w3go/main.go
@@ -3,19 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	arrNum := numbers[7:]
-
-	fmt.Println(arrNum)
-	fmt.Println("len()", len(arrNum))
-	fmt.Println("cap()", cap(arrNum))
-
-	arrNum2 := make([]int, len(arrNum))
-
-	copy(arrNum2, arrNum)
-	fmt.Println("-----------")
-	fmt.Println(arrNum2)
-	fmt.Println("len()", len(arrNum2))
-	fmt.Println("cap()", cap(arrNum2))
-
+	var x = 5
+	var y = 3
+	fmt.Println(x > y)
 }
