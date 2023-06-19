f337516cac1549e0925ce1166cb76ccf175dfa36
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-06-19 17:10:25 +0500

    Update main.go

--- w3go/main.go
+++ w3go/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func factorialRecursion(x float64) (y float64) {
+	if x > 0 {
+		y = x * factorialRecursion(x-1)
+	} else {
+		y = 1
+	}
+	return
+}
 
 func main() {
-	var x = 5
-	var y = 3
-	fmt.Println(x > y)
+	fmt.Println(factorialRecursion(40))
+	
 }
