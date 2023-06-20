d0ddb2a642e20d851e70ec89f8c1fbd6adaf4aca
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-06-20 14:15:53 +0500

    Update main.go

--- w3go/main.go
+++ w3go/main.go
@@ -4,18 +4,39 @@ import (
 	"fmt"
 )
 
-func factorialRecursion(x float64) (y float64) {
-	if x > 0 {
-		y = x * factorialRecursion(x-1)
-	} else {
-		y = 1
-	}
-	return
+type Person struct {
+	name   string
+	age    int
+	job    string
+	salary int
 }
 
 func main() {
-	fmt.Println(factorialRecursion(40))
+	var pers1 Person
+	var pers2 Person
 
+	// Pers1 specification
+	pers1.name = "Hege"
+	pers1.age = 45
+	pers1.job = "Teacher"
+	pers1.salary = 6000
+
+	// Pers2 specification
+	pers2.name = "Cecilie"
+	pers2.age = 24
+	pers2.job = "Marketing"
+	pers2.salary = 4500
+
+	// Print Pers1 info by calling a function
+	printPerson(pers1)
+
+	// Print Pers2 info by calling a function
+	printPerson(pers2)
 }
 
-//https://www.w3schools.com/go/go_struct.php
+func printPerson(pers Person) {
+	fmt.Println("Name: ", pers.name)
+	fmt.Println("Age: ", pers.age)
+	fmt.Println("Job: ", pers.job)
+	fmt.Println("Salary: ", pers.salary)
+}
