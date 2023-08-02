3f40d6c8d5cf760fa9911d80bc17ba4766d6f233
Mirodil <odilfayz22@gmail.com>
2023-08-02 11:31:51 +0500

    Update main.go

--- go_tour/main.go
+++ go_tour/main.go
@@ -11,12 +11,18 @@ func main() {
 
 	b := make([]byte, 8)
 
+	fmt.Println(b)
+
+	fmt.Println("-------------------")
+
 	for {
 		n, err := r.Read(b)
 		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
 		fmt.Printf("b[:n] = %q\n", b[:n])
+		fmt.Println("-------------------")
 		if err == io.EOF {
 			break
 		}
 	}
+	fmt.Printf("b[:n] = %q\n", b)
 }
