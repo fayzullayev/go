ee3bde6e493a31a894585ebf39047b9721d42099
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-08-15 10:37:52 +0500

    Update main.go

--- golangbot/main.go
+++ golangbot/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var age int
-	fmt.Println("My age is", age)
+	a, b := 5.67, 8.97
+	fmt.Printf("type of a %T b %T\n", a, b)
+	sum := a + b
+	diff := a - b
+	fmt.Println("sum", sum, "diff", diff)
+
+	no1, no2 := 56, 89
+	fmt.Println("sum", no1+no2, "diff", no1-no2)
+
 }
