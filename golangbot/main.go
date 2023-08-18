9c2f8977b2415799891ae1a138f1ef44c962e692
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-08-18 18:10:35 +0500

    Update main.go

--- golangbot/main.go
+++ golangbot/main.go
@@ -1,17 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	a, b := 5.67, 8.97
-	fmt.Printf("type of a %T b %T\n", a, b)
-	sum := a + b
-	diff := a - b
-	fmt.Println("sum", sum, "diff", diff)
-
-	no1, no2 := 56, 89
-	fmt.Println("sum", no1+no2, "diff", no1-no2)
+	var a = 5.9 / 8
+	fmt.Printf("a's type is %T and value is %v", a, a)
 
 }
