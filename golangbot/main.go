876a5cece27de281f0b13579df0e3b2a892d7c40
Mirodil <odilfayz22@gmail.com>
2023-08-31 08:19:43 +0500

    Update main.go

--- golangbot/main.go
+++ golangbot/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+	var a [3]int
+
+	fmt.Println("a", a)
+
+	a[2] = 67
+	a[0] = 23
+	a[1] = 1
+
+	fmt.Println("a", a)
+	fmt.Println("len(a)", len(a))
+	fmt.Println("cap(a)", cap(a))
 
 }
