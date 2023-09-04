5e182bb051a0029658cb4987acabcc7b4901ee69
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-09-04 11:01:04 +0500

    Update main.go

--- golangbot/main.go
+++ golangbot/main.go
@@ -1,10 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	a := [...]int{3: 12, 1: 33, 99: 1000}
-	fmt.Println(a)
-	fmt.Println(cap(a))
-	fmt.Println(len(a))
+	a := [3]int{5, 78, 8}
+	var b [5]int
+	b = a
+
 }
