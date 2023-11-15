c26a7818dff31d191ba09cd070a82a95593692ff
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-11-15 14:17:12 +0500

    sqlite

--- base/main.go
+++ base/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"os"
 )
@@ -14,11 +15,8 @@ type Todo struct {
 	Title string `json:"title"`
 }
 
-func DSN() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", os.Getenv("HOST"), os.Getenv("PORT"), os.Getenv("DATABEUSER"), os.Getenv("PASSWORD"), os.Getenv("DBNAME"))
-}
-
 func main() {
+
 	db := initDB()
 	defer func(db *sql.DB) {
 		err := db.Close()
@@ -27,16 +25,29 @@ func main() {
 		}
 	}(db)
 
-	err := db.Ping()
+	rows, err := db.Query("SELECT * FROM todos")
 
 	if err != nil {
-		log.Fatalf("Connection error(Ping) %v", err)
+		log.Fatalf("1. Database error %v", err)
 	}
 
+	var todos []Todo
+
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.Id, &todo.Title)
+
+		if err != nil {
+			log.Fatalf("2. Database error %v", err)
+		}
+		todos = append(todos, todo)
+	}
+
+	fmt.Printf("%+v", todos)
 }
 
 func initDB() *sql.DB {
-	db, err := sql.Open("pgx", DSN())
+	db, err := sql.Open("sqlite3", "./todos.db")
 
 	if err != nil {
 		log.Fatalf("Connection error %v", err)
@@ -51,4 +62,8 @@ func initDB() *sql.DB {
 	fmt.Println("Successfully connected")
 
 	return db
+}
+
+func DSN() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", os.Getenv("HOST"), os.Getenv("PORT"), os.Getenv("DATABASE_USER"), os.Getenv("PASSWORD"), os.Getenv("DBNAME"))
 }
