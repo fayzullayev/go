e483e456cf3b1e25f221f03a34cb8ffd91e8fc2e
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-11-16 14:05:20 +0500

    .

--- base/main.go
+++ base/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/joho/godotenv/autoload"
@@ -24,6 +25,51 @@ type User struct {
 	PhoneNumber string `json:"phoneNumber"`
 }
 
+func (u User) Create(db *sql.DB) (int64, error) {
+	result, err := db.Exec("INSERT INTO users (firstName, lastName, age, job, phoneNumber) VALUES (?, ?, ?, ?, ?)", u.FirstName, u.LastName, u.Age, u.Job, u.PhoneNumber)
+
+	if err != nil {
+		return 0, fmt.Errorf("create user: %v", err)
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, fmt.Errorf("create user: %v", err)
+	}
+
+	num, err := result.RowsAffected()
+
+	if err != nil {
+		return 0, fmt.Errorf("create user: %v", err)
+	}
+
+	fmt.Println("RowsAffected", num)
+
+	return id, nil
+}
+
+func (u User) GetUserById(db *sql.DB, userId int64) (User, error) {
+	//var user User
+	//err := db.QueryRow("SELECT * FROM users WHERE id = ?", userId).Scan(&user.Id, &user.FirstName, &user.LastName, &user.Age, &user.Job, &user.PhoneNumber)
+	//
+	//if err != nil {
+	//	return user, err
+	//}
+	//return user, nil
+	//
+	var user User
+
+	if err := db.QueryRow("SELECT * FROM users WHERE id = ?", userId).Scan(&user.Id, &user.FirstName, &user.LastName, &user.Age, &user.Job, &user.PhoneNumber); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return user, fmt.Errorf("albumsById %d: no such album", userId)
+		}
+		return user, fmt.Errorf("albumsById %d: %v", userId, err)
+	}
+	return user, nil
+
+}
+
 type Album struct {
 	ID     int64
 	Title  string
@@ -41,27 +87,52 @@ func main() {
 		}
 	}(db)
 
-	rows, err := db.Query("SELECT * FROM users")
+	user := User{
+		FirstName:   "Odil",
+		LastName:    "Fayz",
+		Age:         29,
+		Job:         "Dentist",
+		PhoneNumber: "+998909711001",
+	}
+
+	num, err := user.Create(db)
 
 	if err != nil {
-		log.Fatalf("1. Database error %v", err)
+		fmt.Println("errerrerr", err)
 	}
 
-	var todos []User
+	fmt.Println("user created id:", num)
 
-	for rows.Next() {
-		var todo User
-		err = rows.Scan(&todo.Id, &todo.FirstName, &todo.LastName, &todo.Age, &todo.Job, &todo.PhoneNumber)
-
-		if err != nil {
-			log.Fatalf("2. Database error %v", err)
-		}
-		todos = append(todos, todo)
+	user, err = user.GetUserById(db, num)
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
 
-	for i, v := range todos {
-		fmt.Printf("%d. %+v\n", i, v)
-	}
+	fmt.Printf("%+v", user)
+
+	//users, err := getUsers(db, "Journalist")
+	//
+	//if err != nil {
+	//	fmt.Println("err", err)
+	//	return
+	//}
+	//
+	//for i, v := range users {
+	//	fmt.Printf("%d. %+v\n", i, v)
+	//}
+	//
+	//fmt.Println("-----------------------------------")
+	//
+	//user, err := getUserByID(db, 18)
+	//
+	//if err != nil {
+	//	fmt.Println("err", err)
+	//	return
+	//}
+	//
+	//fmt.Printf("User: %+v", user)
+
 }
 
 func initDB() *sql.DB {
@@ -86,25 +157,37 @@ func DSN() string {
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", os.Getenv("HOST"), os.Getenv("PORT"), os.Getenv("DATABASE_USER"), os.Getenv("PASSWORD"), os.Getenv("DBNAME"))
 }
 
-func albumsByArtist(name string, db *sql.DB) ([]Album, error) {
-	// An albums slice to hold data from returned rows.
-	var albums []Album
+func getUsers(db *sql.DB, name string) ([]User, error) {
+	var users []User
 
-	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	rows, err := db.Query("SELECT * FROM users WHERE job = ?", name)
 	if err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		return nil, fmt.Errorf("getUsers %q: %v", name, err)
 	}
 	defer rows.Close()
-	// Loop through rows, using Scan to assign column data to struct fields.
+
 	for rows.Next() {
-		var alb Album
-		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		var user User
+		if err = rows.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Age, &user.Job, &user.PhoneNumber); err != nil {
+			return nil, fmt.Errorf("getUsers %q: %v", name, err)
 		}
-		albums = append(albums, alb)
+		users = append(users, user)
 	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("getUsers %q: %v", name, err)
 	}
-	return albums, nil
+	return users, nil
+}
+
+func getUserByID(db *sql.DB, id int64) (User, error) {
+	// An album to hold data from the returned row.
+	var user User
+
+	if err := db.QueryRow("SELECT * FROM users WHERE id = ?", id).Scan(&user.Id, &user.FirstName, &user.LastName, &user.Age, &user.Job, &user.PhoneNumber); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return user, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return user, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return user, nil
 }
