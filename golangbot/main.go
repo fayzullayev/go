eec71cf9148bf4b0767f945f273725394f13d57d
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-12-01 17:58:20 +0500

    Update main.go

--- golangbot/main.go
+++ golangbot/main.go
@@ -1,5 +1,48 @@
 package main
 
+import "fmt"
+
+type VowelsFinder interface {
+	FindVowels() []rune
+}
+
+type MyString string
+
+func (s MyString) FindVowels() []rune {
+	var vowels []rune
+
+	for _, r := range s {
+		if r == 'a' || r == 'e' || r == 'i' || r == 'o' || r == 'u' {
+			vowels = append(vowels, r)
+		}
+	}
+
+	return vowels
+}
+
 func main() {
+
+	//name := MyString("Mirodil Fayzullayev")
+	//
+	//var v VowelsFinder
+	//
+	//v = name
+	//
+	//fmt.Printf("Vowels are %c\n", v.FindVowels())
+
+	//fmt.Printf("%v, %x, %08b, %c --- %08b\n", '💕', '💕', '💕', '💕', "💕")
+
+	str := "💕"
+	StringToBinary(str)
+	//fmt.Println("Binary representation:", binary)
+
+}
+
+func StringToBinary(s string) {
+
+	for _, c := range s {
+		fmt.Println(1)
+		fmt.Printf("%08b\n", c)
+	}
 
 }
