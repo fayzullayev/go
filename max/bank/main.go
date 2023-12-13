41c9f497996c95a600563b8d73bade599e659902
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-12-13 19:20:08 +0500

    Update main.go

--- max/bank/main.go
+++ max/bank/main.go
@@ -25,19 +25,57 @@ mainLoop:
 		fmt.Print("Your choice: ")
 		_, err := fmt.Scan(&choice)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal(1, err)
 		}
 
-		switch choice {
-		case 1:
-			fmt.Println("Your balance:", accountBalance)
-		case 2:
-		case 3:
-		case 4:
+		if choice == 1 {
+			fmt.Println("-----------------------")
+			fmt.Println("Your balance is", accountBalance)
+		} else if choice == 2 {
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			_, err = fmt.Scan(&depositAmount)
+			if err != nil {
+				log.Fatal(2, err)
+			}
+			if depositAmount <= 0 {
+				fmt.Println("-----------------------")
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+		} else if choice == 3 {
+			fmt.Print("Withdrawal amount: ")
+			var withdrawalAmount float64
+			_, err = fmt.Scan(&withdrawalAmount)
+			if err != nil {
+				log.Fatal(3, err)
+			}
+
+			if withdrawalAmount <= 0 || withdrawalAmount > accountBalance {
+				fmt.Println("-----------------------")
+				fmt.Println("Invalid amount.")
+				continue
+			}
+			accountBalance -= withdrawalAmount
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+		} else {
 			break mainLoop
-		default:
-
 		}
+
+		//switch choice {
+		//case 1:
+		//	fmt.Println("-----------------------")
+		//	fmt.Println("Your balance:", accountBalance)
+		//case 2:
+		//case 3:
+		//case 4:
+		//	break mainLoop
+		//default:
+		//	fmt.Println("-----------------------")
+		//	fmt.Println("Wrong choice")
+		//}
 	}
 
 	fmt.Println("Thank you for your operation")
