fce22d17f5e5ec05b59a9579137cd0735df92c2b
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-12-17 23:02:42 +0500

    Update main.go

--- max/struct/main.go
+++ max/struct/main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+type user struct {
+	firstName, lastName, birthDate string
+	age                            int
+	createdAt                      time.Time
+}
 
 func main() {
 	firstName := getUserData("Please enter your first name: ")
 	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	birthDate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser user
+
+	appUser = user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthDate,
+		createdAt: time.Now(),
+	}
+
+	fmt.Println()
 
 	// ... do something awesome with that gathered data!
 
-	fmt.Println(firstName, lastName, birthdate)
+	outPutUserDetails(&appUser)
+}
+
+func outPutUserDetails(u *user) {
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
 }
 
 func getUserData(promptText string) string {
