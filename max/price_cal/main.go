cb8365156ead3b730925204e77f599ea4591b67f
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-01-12 18:16:27 +0500

    Update main.go

--- max/price_cal/main.go
+++ max/price_cal/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	prices := []float64{10, 20, 30}
+
+	var taxRates = []float64{0, 0.7, 0.1, 0.15}
+
+	var result = make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+
+		var taxIncludedPrices = make([]float64, len(prices))
+
+		for priceIndex, price := range prices {
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+		}
+
+		result[taxRate] = taxIncludedPrices
+	}
+	fmt.Printf("%+v", result)
 }
