e801aa79632b654ab378b47b927bdfb089f9edb7
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-01-13 17:45:52 +0500

    .

--- max/arrays/main.go
+++ max/arrays/main.go
@@ -4,26 +4,13 @@ import "fmt"
 
 func main() {
 
-	//websites := make(map[string]string)
-	var websites map[string]string = map[string]string{
-		"google": "https://wwww.google2.com",
-	}
+	//names := make([]string, 5)
+	var names = make([]string, 3)
+	fmt.Println(names, len(names), cap(names))
 
-	fmt.Println(websites)
+	names = append(names, "Mirodil", "Zayab")
+	//names = append(names, "Muhammad", "Umar" )
 
-	websites["google"] = "https://wwww.google.com"
-	websites["amazon"] = "https://wwww.aws.com"
-	websites["facebook"] = "https://wwww.facebook.com"
-
-	fmt.Printf("%+v\n", websites)
-
-	value, ok := websites["amazon2"]
-	fmt.Println(value, ok)
-
-	delete(websites, "facebook")
-
-	for k, v := range websites {
-		fmt.Println(k, v)
-	}
+	fmt.Println(names, len(names), cap(names))
 
 }
