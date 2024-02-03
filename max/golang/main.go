c23e001d556ebe9dde163bf012bcac1a005a687b
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-02-03 19:01:06 +0500

    Update main.go

--- max/golang/main.go
+++ max/golang/main.go
@@ -8,24 +8,26 @@ import (
 func greet(phrase string, d chan bool) {
 	fmt.Println("Hello!", phrase)
 	d <- true
+
 }
 
 func slowGreet(phrase string, d chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	d <- true
+	close(d)
 }
 
 func main() {
-	done := make(chan bool, 4)
+	done := make(chan bool)
 
 	go greet("Nice to meet you!", done)
 	go greet("How are you?", done)
 	go slowGreet("How ... are ... you ...?", done)
 	go greet("I hope you're liking the course!", done)
 
-	for d := range done {
-		fmt.Println(d)
+	for range done {
+
 	}
 	//fmt.Println(<-done)
 	//fmt.Println(<-done)
