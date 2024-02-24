5162738d38a88b7bb322d8e853882417e2c5eb4d
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-02-24 12:58:48 +0500

    Update main.go

--- stephen/interface/main.go
+++ stephen/interface/main.go
@@ -2,6 +2,39 @@ package main
 
 import "fmt"
 
+type Greeter interface {
+	Greet() string
+}
+
+type EnglishBot struct {
+	Text string
+}
+
+func (eb EnglishBot) Greet() string {
+	return eb.Text
+}
+
+type SpanishBot struct {
+	Text string
+}
+
+func (eb SpanishBot) Greet() string {
+	return eb.Text
+}
+
 func main() {
-	fmt.Println("Hello!")
+	eb := EnglishBot{
+		"Hello",
+	}
+
+	sb := SpanishBot{
+		Text: "Hola!",
+	}
+
+	Greeting(eb)
+	Greeting(sb)
+}
+
+func Greeting(gr Greeter) {
+	fmt.Println(gr.Greet())
 }
