5216656f20d1fb0bdaae151afa6dced7e723766e
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-03-01 21:56:40 +0500

    Update main.go

--- stephen/concurrency/main.go
+++ stephen/concurrency/main.go
@@ -1,38 +1,67 @@
+// package main
+//
+// import (
+//
+//	"fmt"
+//	"net/http"
+//	"time"
+//
+// )
+//
+//	func main() {
+//		links := []string{
+//			"https://google.com",
+//			"https://facebook.com",
+//			"https://stackoverflow.com",
+//			"https://amazon.com",
+//			"https://golang.org",
+//		}
+//
+//		var ch = make(chan string)
+//
+//		for _, link := range links {
+//			go checkLink(link, ch)
+//		}
+//
+//		for l := range ch {
+//			go func(link string) {
+//				time.Sleep(time.Second * 3)
+//				checkLink(link, ch)
+//			}(l)
+//		}
+//
+// }
+//
+// func checkLink(link string, ch chan string) {
+//
+//		_, err := http.Get(link)
+//
+//		if err != nil {
+//			fmt.Println(link, " might be down")
+//			ch <- link
+//			return
+//		}
+//
+//		fmt.Println(link, " is up")
+//		ch <- link
+//	}
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	links := []string{
-		"https://google.com",
-		"https://facebook.com",
-		"https://stackoverflow.com",
-		"https://amazon.com",
-		"https://golang.org",
-	}
+	c := make(chan string)
 
-	var ch = make(chan string)
-
-	for _, link := range links {
-		go checkLink(link, ch)
+	for i := 0; i < 4; i++ {
+		go printString("Hello there!", c)
 	}
 
 	for {
-		fmt.Println(<-ch)
+		fmt.Println(<-c)
 	}
-
 }
 
-func checkLink(link string, ch chan string) {
-	_, err := http.Get(link)
-
-	if err != nil {
-		ch <- link + " might be down!"
-		return
-	}
-
-	ch <- link + " is up"
+func printString(s string, c chan string) {
+	fmt.Println(s)
+	c <- "Done printing."
 }
