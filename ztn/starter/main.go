cb53f1d0ae4355b19b2d4b25a89b503f744ca8dd
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-03-12 16:01:59 +0500

    .

--- ztn/starter/main.go
+++ ztn/starter/main.go
@@ -1,9 +1,24 @@
 package main
 
-import (
-	util "starter/utils"
-)
+import "fmt"
 
 func main() {
-	util.Hello()
+	for i := range 100 {
+		isFizzBuzz(i)
+	}
+}
+
+func isFizzBuzz(number int) {
+	isDivisableBy3 := number%3 == 0
+	isDivisableBy5 := number%5 == 0
+
+	if isDivisableBy3 && isDivisableBy5 {
+		fmt.Println(number, "FizzBuzz")
+	} else if isDivisableBy3 {
+		fmt.Println(number, "Fizz")
+	} else if isDivisableBy5 {
+		fmt.Println(number, "Buzz")
+	} else {
+		fmt.Println(number)
+	}
 }
