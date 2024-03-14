7fea75ce64a8245a6f7680acc2944754f854676d
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-03-14 17:53:33 +0500

    Update main.go

--- ztn/starter/main.go
+++ ztn/starter/main.go
@@ -1,29 +1,12 @@
 package main
 
-import "fmt"
+const (
+	Online = iota
+	Offline
+	Maintenance
+	Retired
+)
 
 func main() {
-	routes := []string{"Grocery", "Department Store", "Salon"}
 
-	printSlice("Route 1", routes)
-
-	routes = append(routes, "Home")
-	printSlice("Route 2", routes)
-	fmt.Println()
-
-	fmt.Println(routes[0], "visited")
-	fmt.Println(routes[1], "visited")
-
-	routes = routes[2:]
-	printSlice("Remaining locations", routes)
-
-}
-
-func printSlice(title string, slice []string) {
-	fmt.Println()
-	fmt.Println("---", title, "---")
-
-	for i := 0; i < len(slice); i++ {
-		fmt.Println(slice[i])
-	}
 }
