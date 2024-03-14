c3c69cc1f32b6daebdf496161bd74832b5263953
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-03-14 15:20:04 +0500

    Update main.go

--- ztn/starter/main.go
+++ ztn/starter/main.go
@@ -2,46 +2,28 @@ package main
 
 import "fmt"
 
-type Product struct {
-	price int
-	name  string
-}
-
-func printStats(products [4]Product) {
-	var cost, totalItem int
-
-	for i := 0; i < len(products); i++ {
-		product := products[i]
-		cost += product.price
-
-		if product.name != "" {
-			totalItem += 1
-		}
-	}
-
-	fmt.Println("cost: ", cost)
-	fmt.Println("number of items: ", totalItem)
-	fmt.Println("last  of item: ", products[totalItem-1])
-
-}
-
 func main() {
+	routes := []string{"Grocery", "Department Store", "Salon"}
 
-	shoppingList := [4]Product{
-		{1, "Banana"},
-		{4, "Apple"},
-		//{6, "Orange"},
-		{18, "Lemon"},
+	printSlice("Route 1", routes)
+
+	routes = append(routes, "Home")
+	printSlice("Route 2", routes)
+	fmt.Println()
+
+	fmt.Println(routes[0], "visited")
+	fmt.Println(routes[1], "visited")
+
+	routes = routes[2:]
+	printSlice("Remaining locations", routes)
+
+}
+
+func printSlice(title string, slice []string) {
+	fmt.Println()
+	fmt.Println("---", title, "---")
+
+	for i := 0; i < len(slice); i++ {
+		fmt.Println(slice[i])
 	}
-
-	printStats(shoppingList)
-
-	shoppingList[3] = Product{
-		price: 50,
-		name:  "Notebook",
-	}
-	fmt.Println("----------------")
-
-	printStats(shoppingList)
-
 }
