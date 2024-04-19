efa993d391277ae9984206d52767357c3017dd0f
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-04-19 17:35:33 +0500

    .

--- templates/golang+temple/cmd/routes.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func RegisterRouter(app *App) {
-	app.Router.GET("/users", app.Handlers.UsersHandler.GetUsers)
-	app.Router.GET("/tasks", app.Handlers.TasksHandler.GetTasks)
-}
--- templates/golang+temple/handler/users.go
+++ templates/golang+temple/handler/users.go
@@ -2,8 +2,10 @@ package handler
 
 import (
 	"database/sql"
+	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"tempaltes-tutorial/view"
 )
 
 type UsersHandler struct {
@@ -13,6 +15,23 @@ type UsersHandler struct {
 func NewUsersHandler(db *sql.DB) *UsersHandler {
 	return &UsersHandler{
 		DB: db,
+	}
+}
+
+func render(c *gin.Context, status int, template templ.Component) error {
+
+	c.Status(status)
+	return template.Render(c.Request.Context(), c.Writer)
+}
+
+func (u *UsersHandler) Home(c *gin.Context) {
+	err := render(c, 200, view.Hello("Mirodil"))
+
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "error",
+		})
+		return
 	}
 }
 
