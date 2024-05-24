4f35a6440d7f9b1b4cd75cb0456eedb9c6e27afe
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-05-24 16:42:22 +0500

    .

---
+++ go-mastering/start/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	aSlice := make([]int, 4, 4)
+	fmt.Println(aSlice)
+
+	// The capacity is doubled
+	fmt.Println("L:", len(aSlice), "C:", cap(aSlice)) // Now add four elements
+	aSlice = append(aSlice, []int{-1, -2, -3, -4, 6}...)
+
+	fmt.Println(aSlice)
+	// The capacity is doubled
+	fmt.Println("L:", len(aSlice), "C:", cap(aSlice))
+}
