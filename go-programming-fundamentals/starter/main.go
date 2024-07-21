b2cebd2c8d0026c28823b20f6fecbc9eb6ff4d5a
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-07-21 23:22:31 +0500

    Update main.go

--- go-programming-fundamentals/starter/main.go
+++ go-programming-fundamentals/starter/main.go
@@ -1,10 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"github.com/fayzullayev/go-fundamentals/utils"
-)
+import "fmt"
+
+type Vehicle struct {
+	Make  string
+	Model string
+	Year  int
+}
+
+type Car struct {
+	Vehicle
+	BodyStyle string
+	Year      int
+}
 
 func main() {
-	fmt.Println(utils.Add(11, 22))
+
+	var myCar Car = Car{
+		Vehicle: Vehicle{
+			Make:  "gerg",
+			Model: "gerg",
+			Year:  0,
+		},
+		BodyStyle: "dasfasf",
+		Year:      2000,
+	}
+
+	fmt.Println(myCar.Vehicle.Year)
+
 }
