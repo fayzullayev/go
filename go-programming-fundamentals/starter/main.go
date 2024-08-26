a727236e7875b5b80020b521aed6c5340401cde8
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-08-26 18:05:06 +0500

    Update main.go

--- go-programming-fundamentals/starter/main.go
+++ go-programming-fundamentals/starter/main.go
@@ -2,22 +2,56 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"reflect"
-	"strings"
 )
 
-type Person struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-	Addr string `json:"addr,omitempty"`
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c *Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Rectangle struct {
+	Height, Width float64
+}
+
+func (r *Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (r *Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
 }
 
 func main() {
+	c := &Circle{10}
+	r := &Rectangle{10, 20}
 
-	t := reflect.TypeOf(Person{})
+	fmt.Println(TotalArea(c, r))
 
-	field, _ := t.FieldByName("Addr")
+}
 
-	fmt.Println(strings.Split(field.Tag.Get("json"), ",")[1])
+func TotalArea(shapes ...Shape) float64 {
+	var total float64
+
+	fmt.Println(reflect.TypeOf(shapes))
+
+	for _, shape := range shapes {
+		total += shape.Area()
+	}
+
+	return total
 
 }
