f236d243b323af319288a6f7f143483d6f6f9123
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-09-20 18:30:26 +0500

    Update main.go

--- purple school/starter/main.go
+++ purple school/starter/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -10,38 +9,42 @@ import (
 )
 
 func main() {
+	var transactions []float64
+
 	reader := bufio.NewReader(os.Stdin)
 
+	fmt.Println("Welcome to the transactions parser")
+
 	for {
-		fmt.Print("Enter a number: ")
-		readString, err := reader.ReadString('\n')
+		fmt.Print("Enter amount of transactions: ")
+		line, err := reader.ReadString('\n')
 		if err != nil {
-			//fmt.Println("Error reading input-1:", err)
-			panic("vsdvsdvsdv")
+			panic("Something went wrong!")
+		}
+
+		stringNumber := strings.ReplaceAll(strings.TrimSpace(line), "\n", "")
+
+		floatNumber, err := strconv.ParseFloat(stringNumber, 64)
+		if err != nil {
+			panic("Error parsing!")
+		}
+
+		transactions = append(transactions, floatNumber)
+
+		fmt.Print("Do you want to continue? (y/n): ")
+		line, err = reader.ReadString('\n')
+		if err != nil {
+			panic("Something went wrong!")
+		}
+
+		line = strings.ReplaceAll(strings.TrimSpace(line), "\n", "")
+
+		if !(line == "Y" || line == "y") {
 			break
 		}
-
-		errors.New("wefwefwefwefwef")
-
-		num, err := strconv.Atoi(strings.ReplaceAll(readString, "\n", ""))
-		if err != nil {
-			fmt.Println("Error parsing input-2:", err)
-			return
-		}
-		fmt.Printf("%d - %T \n --------------------- \n", num, num)
-
-		fmt.Print("Do you want to continue: ")
-		prompt, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Error reading input:", err)
-			break
-		}
-
-		if strings.ToLower(strings.TrimSpace(strings.ReplaceAll(prompt, "\n", ""))) != "y" {
-			break
-		}
-
 	}
 
-	fmt.Println("Done")
+	fmt.Println("Transactions: ", transactions)
+	fmt.Println("Total transactions: ", len(transactions))
+
 }
