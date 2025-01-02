fd5a247f294b102a77be1330df7655427e55bd70
Mirodil Fayzullayev <odilfayz22@gmail.com>
2025-01-02 22:25:35 +0500

    .

--- w3/main.go
+++ w3/main.go
@@ -1,7 +1,18 @@
+//https://www.w3schools.com/go/go_arrays.php
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello!!!")
+	var txt1 string = "Hello"
+	var txt2 string
+	txt3 := "World 1"
+
+	fmt.Printf("Type: %T, value: %s\n", txt1, txt1)
+	fmt.Printf("Type: %T, value: %s\n", txt2, txt2)
+	fmt.Printf("Type: %T, value: %s\n", txt3, txt3[len(txt3)-1:])
+
 }
